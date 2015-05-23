b8111622a1234037e7843c81c22d9c9078c8a49c
eighty_cent <471840065@qq.com>
2015-05-23 23:31:43 +0800

    Initial Commit

---
+++ 聚餐记账/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  聚餐记账
+//
+//  Created by apple on 15/5/23.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
